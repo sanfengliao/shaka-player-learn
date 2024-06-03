2321bc0184bdb43aad1442637eb19100861c9d7b
sanfengliao <sanfengliao@gmail.com>
2024-06-04 00:35:59 +0800

    feat: 实现publicPromise

---
+++ src/lib/util/public_promise.ts
@@ -0,0 +1,38 @@
+/**
+ * @summary
+ * A utility to create Promises with convenient public resolve and reject
+ * methods.
+ *
+ */
+export class PublicPromise<T> {
+  constructor() {
+    let resolvePromise!: (value: T | PromiseLike<T>) => void;
+    let rejectPromise!: (reason?: any) => void;
+
+    // Promise.call causes an error.  It seems that inheriting from a native
+    // Promise is not permitted by JavaScript interpreters.
+
+    // The work-around is to construct a Promise object, modify it to look like
+    // the compiler's picture of PublicPromise, then return it.  The caller of
+    // new PublicPromise will receive |promise| instead of |this|, and the
+    // compiler will be aware of the additional properties |resolve| and
+    // |reject|.
+
+    const promise = new Promise<T>((resolve, reject) => {
+      resolvePromise = resolve;
+      rejectPromise = reject;
+    });
+
+    // Now cast the Promise object to our subclass PublicPromise so that the
+    // compiler will permit us to attach resolve() and reject() to it.
+    const publicPromise = promise as unknown as PublicPromise<T>;
+    publicPromise.resolve = resolvePromise;
+    publicPromise.reject = rejectPromise;
+
+    return publicPromise;
+  }
+
+  resolve(value: T) {}
+
+  reject(reason: T) {}
+}
