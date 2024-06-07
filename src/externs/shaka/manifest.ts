2273c98107766b79679f3b30bbe688a6a032a43a
sanfengliao <sanfengliao@gmail.com>
2024-06-07 23:06:21 +0800

    feat: manifest接口完善

--- src/externs/shaka/manifest.ts
+++ src/externs/shaka/manifest.ts
@@ -1,3 +1,4 @@
+import { AccessibilityPurpose } from '../../lib/media/manifest_parser';
 import { PresentationTimeline } from '../../lib/media/presentation_timeline';
 import { SegmentIndex } from '../../lib/media/segment_index';
 import { SegmentReference } from '../../lib/media/segment_reference';
@@ -151,18 +152,80 @@ export interface InitDataOverride {
   keyId?: string;
 }
 
+/**
+ * @description
+ * DRM configuration for a single key system.
+ */
 export interface DrmInfo {
+  /**
+   * <i>Required.</i> <br>
+   * The key system, e.g., "com.widevine.alpha".
+   */
   keySystem: string;
+  /**
+   * <i>Required.</i> <br>
+   * The encryption scheme, e.g., "cenc", "cbcs", "cbcs-1-9".
+   */
   encryptionScheme: string;
+  /**
+   * <i>Filled in by DRM config if missing.</i> <br>
+   * The license server URI.
+   */
   licenseServerUri: string;
+  /**
+   * <i>Defaults to false.  Can be filled in by advanced DRM config.</i> <br>
+   * True if the application requires the key system to support distinctive
+   * identifiers.
+   */
   distinctiveIdentifierRequired: boolean;
+  /**
+   * <i>Defaults to false.  Can be filled in by advanced DRM config.</i> <br>
+   * True if the application requires the key system to support persistent
+   * state, e.g., for persistent license storage.
+   */
   persistentStateRequired: boolean;
-  audioRobustness: string;
-  videoRobustness: string;
-  serverCertificate: Uint8Array | null;
-  serverCertificateUri: string;
+  /**
+   * <i>Defaults to 'temporary' if Shaka wasn't initiated for storage.
+   * Can be filled in by advanced DRM config sessionType parameter.</i> <br>
+   */
   sessionType: string;
+  /**
+   * <i>Defaults to '', e.g., no specific robustness required.  Can be filled in
+   * by advanced DRM config.</i> <br>
+   * A key-system-specific string that specifies a required security level.
+   */
+  audioRobustness: string;
+  /**
+   * <i>Defaults to '', e.g., no specific robustness required.  Can be filled in
+   * by advanced DRM config.</i> <br>
+   * A key-system-specific string that specifies a required security level.
+   */
+  videoRobustness: string;
+  /**
+   * <i>Defaults to null, e.g., certificate will be requested from the license
+   * server if required.  Can be filled in by advanced DRM config.</i> <br>
+   * A key-system-specific server certificate used to encrypt license requests.
+   * Its use is optional and is meant as an optimization to avoid a round-trip
+   * to request a certificate.
+   */
+  serverCertificate: Uint8Array | null;
+  /**
+   * <i>Defaults to '', e.g., server certificate will be requested from the
+   * given URI if serverCertificate is not provided. Can be filled in by
+   * advanced DRM config.</i>
+   */
+  serverCertificateUri: string;
+  /**
+   * <i>Defaults to [], e.g., no override.</i> <br>
+   * A list of initialization data which override any initialization data found
+   * in the content.  See also shaka.extern.InitDataOverride.
+   */
   initData: Array<InitDataOverride>;
+  /**
+   * <i>Defaults to the empty Set</i> <br>
+   * If not empty, contains the default key IDs for this key system, as
+   * lowercase hex strings.
+   */
   keyIds: Set<string>;
 }
 
@@ -178,46 +241,224 @@ type CreateSegmentIndexFunction = () => Promise<any>;
  * @description A Stream object describes a single stream (segmented media data).
  */
 export interface Stream {
+  /**
+   * <i>Required.</i> <br>
+   * A unique ID among all Stream objects within the same Manifest.
+   */
   id: number;
+  /**
+   * <i>Optional.</i> <br>
+   * The original ID, if any, that appeared in the manifest.  For example, in
+   * DASH, this is the "id" attribute of the Representation element.  In HLS,
+   * this is the "NAME" attribute.
+   */
   originalId?: string;
+  /**
+   * <i>Optional.</i> <br>
+   * The ID of the stream's parent element. In DASH, this will be a unique
+   * ID that represents the representation's parent adaptation element
+   */
   groupId?: string;
+  /**
+   * <i>Required.</i> <br>
+   * Creates the Stream's segmentIndex (asynchronously).
+   */
   createSegmentIndex: CreateSegmentIndexFunction;
+  /**
+   * <i>Optional.</i> <br>
+   * Closes the Stream's segmentIndex.
+   */
   closeSegmentIndex: () => void;
-
+  /**
+   * <i>Required.</i> <br>
+   * May be null until createSegmentIndex() is complete.
+   */
   segmentIndex: SegmentIndex;
+  /**
+   * The Stream's MIME type, e.g., 'audio/mp4', 'video/webm', or 'text/vtt'.
+   * In the case of a stream that adapts between different periods with
+   * different MIME types, this represents only the first period.
+   */
   mimeType: string;
+  /**
+   * <i>Defaults to '' (i.e., unknown / not needed).</i> <br>
+   * The Stream's codecs, e.g., 'avc1.4d4015' or 'vp9', which must be
+   * compatible with the Stream's MIME type. <br>
+   * In the case of a stream that adapts between different periods with
+   * different codecs, this represents only the first period.
+   * See {@link https://tools.ietf.org/html/rfc6381}
+   */
   codecs: string;
-  frameRate: number | undefined;
-  pixelAspectRatio: string | undefined;
-  hdr: string | undefined;
+  /**
+   * <i>Video streams only.</i> <br>
+   * The Stream's framerate in frames per second
+   */
+  frameRate?: number;
+  /**
+   * <i>Video streams only.</i> <br>
+   * The Stream's pixel aspect ratio
+   */
+  pixelAspectRatio?: string;
+  /**
+   * <i>Video streams only.</i> <br>
+   * The Stream's HDR info
+   */
+  hdr?: string;
+  /**
+   * <i>Video streams only.</i> <br>
+   * The Stream's color gamut info
+   */
+  colorGamut?: string;
+  /**
+   * <i>Video streams only.</i> <br>
+   * The Stream's video layout info.
+   */
   videoLayout: string | undefined;
-  bandwidth: number | undefined;
-  width: number | undefined;
-  height: number | undefined;
-  kind: string | undefined;
+  /**
+   * <i>Audio and video streams only.</i> <br>
+   * The stream's required bandwidth in bits per second.
+   */
+  bandwidth?: number;
+  /**
+   * <i>Video streams only.</i> <br>
+   * The stream's width in pixels.
+   */
+  width?: number;
+  /**
+   * <i>Video streams only.</i> <br>
+   * The stream's height in pixels.
+   */
+  height?: number;
+  /**
+   * <i>Text streams only.</i> <br>
+   * The kind of text stream.  For example, 'caption' or 'subtitle'.
+   * @see https://bit.ly/TextKind
+   */
+  kind?: string;
+  /**
+   * <i>Defaults to false.</i><br>
+   * True if the stream is encrypted.
+   */
   encrypted: boolean;
-  drmInfos: Array<DrmInfo>;
+  /**
+   * <i>Defaults to [] (i.e., no DRM).</i> <br>
+   * An array of DrmInfo objects which describe DRM schemes are compatible with
+   * the content.
+   */
+  drmInfos: DrmInfo[];
+  /**
+   * <i>Defaults to empty (i.e., unencrypted or key ID unknown).</i> <br>
+   * The stream's key IDs as lowercase hex strings. These key IDs identify the
+   * encryption keys that the browser (key system) can use to decrypt the
+   * stream.
+   */
   keyIds: Set<string>;
+  /**
+   * The Stream's language, specified as a language code. <br>
+   * Audio stream's language must be identical to the language of the containing
+   * Variant.
+   */
   language: string;
+  /**
+   * <i>Optional.</i> <br>
+   * The original language, if any, that appeared in the manifest.
+   */
   originalLanguage?: string;
+  /**
+   * The Stream's label, unique text that should describe the audio/text track.
+   */
   label?: string;
+  /**
+   * <i>Required.</i> <br>
+   * Content type (e.g. 'video', 'audio' or 'text', 'image')
+   */
   type: string;
+  /**
+   * <i>Defaults to false.</i> <br>
+   * True indicates that the player should use this Stream over others if user
+   * preferences cannot be met.  The player may still use another Variant to
+   * meet user preferences.
+   */
   primary: boolean;
+  /**
+   * <i>Video streams only.</i> <br>
+   * An alternate video stream to use for trick mode playback.
+   */
   trickModeVideo?: Stream;
-  emsgSchemeIdUris?: Array<string>;
-  roles: Array<string>;
-  // TODO
-  accessibilityPurpose?: shaka.media.ManifestParser.AccessibilityPurpose;
+  /**
+   * <i>Defaults to empty.</i><br>
+   * Array of registered emsg box scheme_id_uri that should result in
+   * Player events.
+   */
+  emsgSchemeIdUris?: string[];
+  /**
+   * The roles of the stream as they appear on the manifest,
+   * e.g. 'main', 'caption', or 'commentary'.
+   */
+  roles: string[];
+  /**
+   * accessibilityPurpose
+   * The DASH accessibility descriptor, if one was provided for this stream.
+   */
+  accessibilityPurpose?: AccessibilityPurpose;
+  /**
+   * <i>Defaults to false.</i> <br>
+   * Whether the stream set was forced
+   */
   forced: boolean;
+  /**
+   * The channel count information for the audio stream.
+   */
   channelsCount?: number;
+  /**
+   * Specifies the maximum sampling rate of the content.
+   */
   audioSamplingRate?: number;
+  /**
+   * <i>Defaults to false.</i> <br>
+   * Whether the stream set has spatial audio
+   */
   spatialAudio: boolean;
+  /**
+   * A map containing the description of closed captions, with the caption
+   * channel number (CC1 | CC2 | CC3 | CC4) as the key and the language code
+   * as the value. If the channel number is not provided by the description,
+   * we'll set a 0-based index as the key. If the language code is not
+   * provided by the description we'll set the same value as channel number.
+   * Example: {'CC1': 'eng'; 'CC3': 'swe'}, or {'1', 'eng'; '2': 'swe'}, etc.
+   */
   closedCaptions: Map<string, string>;
+  /**
+   * <i>Image streams only.</i> <br>
+   * The value is a grid-item-dimension consisting of two positive decimal
+   * integers in the format: column-x-row ('4x3'). It describes the arrangement
+   * of Images in a Grid. The minimum valid LAYOUT is '1x1'.
+   */
   tilesLayout?: string;
+  /**
+   * The streams in all periods which match the stream. Used for Dash.
+   */
   matchedStreams?: Array<Stream> | Array<StreamDB>;
+  /**
+   * <i>Microsoft Smooth Streaming only.</i> <br>
+   * Private MSS data that is necessary to be able to do transmuxing.
+   */
   mssPrivateData?: MssPrivateData;
+  /**
+   * Indicate if the stream was added externally.
+   * Eg: external text tracks.
+   */
   external: boolean;
+  /**
+   * Indicate if the stream should be used for fast switching.
+   */
   fastSwitching: boolean;
+  /**
+   * A set of full MIME types (e.g. MIME types plus codecs information), that
+   * represents the types used in each period of the original manifest.
+   * Meant for being used by compatibility checking, such as with
+   * MediaSource.isTypeSupported.
+   */
   fullMimeTypes: Set<string>;
 }
 
@@ -285,30 +526,20 @@ export interface Variant {
   decodingInfos: Array<MediaCapabilitiesDecodingInfo>;
 }
 
-/**
- * @typedef {{
- *   duration: number,
- *   timescale: number,
- *   codecPrivateData: ?string
- * }}
- *
- * @description
- * Private MSS data that is necessary to be able to do transmuxing.
- *
- * @property {number} duration
- *   <i>Required.</i> <br>
- *   MSS Stream duration.
- * @property {number} timescale
- *   <i>Required.</i> <br>
- *   MSS timescale.
- * @property {?string} codecPrivateData
- *   MSS codecPrivateData.
- *
- * @exportDoc
- */
 export interface MssPrivateData {
+  /**
+   * <i>Required.</i> <br>
+   * MSS Stream duration.
+   */
   duration: number;
+  /**
+   * <i>Required.</i> <br>
+   * MSS timescale.
+   */
   timescale: number;
+  /**
+   * MSS codecPrivateData.
+   */
   codecPrivateData?: string;
 }
 
---
+++ src/lib/media/manifest_parser.ts
@@ -0,0 +1,4 @@
+export const enum AccessibilityPurpose {
+  VISUALLY_IMPAIRED = 'visually impaired',
+  HARD_OF_HEARING = 'hard of hearing',
+}
